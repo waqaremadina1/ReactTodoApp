c3576e630c19ba055339f718e4d5caa96b98805c
Waqar Bin Abrar <31979294+waqaremadina1@users.noreply.github.com>
2024-12-31 17:44:57 +0500

    first commit

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Frontend from './Frontend'
-import Authentication from './Authentication'
-import Dashboard from './Dashboard'
-import { useAuthContext } from '../context/AuthContext'
-import PrivatRoute from '../components/PrivatRoute'
+// import React from 'react'
+// import { Navigate, Route, Routes } from 'react-router-dom'
+// import Frontend from './Frontend'
+// import Authentication from './Authentication'
+// import Dashboard from './Dashboard'
+// import { useAuthContext } from '../context/AuthContext'
+// import PrivatRoute from '../components/PrivatRoute'
 
 
-export default function Index() {
-    const { isAuthenticated, user } = useAuthContext()
+// export default function Index() {
+//     const { isAuthenticated, user } = useAuthContext()
 
   
 
-    return (
-        <Routes>
-            <Route path='/*' element={<Frontend />} />
-            <Route path="/authentication/*" element={!isAuthenticated ? <Authentication /> : <Navigate to="/" />} />
-            <Route path='/dashboard/*' element={<PrivatRoute Component={Dashboard} />} />
+//     return (
+//         <Routes>
+//             <Route path='/*' element={<Frontend />} />
+//             <Route path="/authentication/*" element={!isAuthenticated ? <Authentication /> : <Navigate to="/" />} />
+//             <Route path='/dashboard/*' element={<PrivatRoute Component={Dashboard} />} />
             
 
 
 
-        </Routes>
-    )
-}
+//         </Routes>
+//     )
+// }
