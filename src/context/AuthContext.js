c75038174d8dc94c538569f23c7297eb9bc65743
Waqar Bin Abrar <31979294+waqaremadina1@users.noreply.github.com>
2024-12-31 17:44:29 +0500

    first commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,52 +1,52 @@
-import { onAuthStateChanged } from 'firebase/auth'
-import React, { createContext, useContext, useEffect, useReducer, useState } from 'react'
-import { auth } from '../config/firebase'
+// import { onAuthStateChanged } from 'firebase/auth'
+// import React, { createContext, useContext, useEffect, useReducer, useState } from 'react'
+// import { auth } from '../config/firebase'
 
-export const Auth = createContext()
-const initialState = { isAuthenticated: false, user: {} }
+// export const Auth = createContext()
+// const initialState = { isAuthenticated: false, user: {} }
 
-const reducer = ((state, { type, payload }) => {
-    switch (type) {
-        case "LOGIN":
-            return { isAuthenticated: true, user: payload.user }
-        case "LOGOUT":
-            return { ...initialState }
-        default:
-            return state
-    }
-})
+// const reducer = ((state, { type, payload }) => {
+//     switch (type) {
+//         case "LOGIN":
+//             return { isAuthenticated: true, user: payload.user }
+//         case "LOGOUT":
+//             return { ...initialState }
+//         default:
+//             return state
+//     }
+// })
 
-export default function AuthContext({ children }) {
+// export default function AuthContext({ children }) {
 
-    const [isAppLoader, setIsAppLoader] = useState(false)
+//     const [isAppLoader, setIsAppLoader] = useState(false)
 
-    useEffect(() => {
-        setIsAppLoader(true)
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // window.toastify("user is signed in", "success")
-                dispatch({ type: "LOGIN", payload: { user } })
-            } else {
-                dispatch({ type: "LOGOUT" })
+//     useEffect(() => {
+//         setIsAppLoader(true)
+//         onAuthStateChanged(auth, (user) => {
+//             if (user) {
+//                 // window.toastify("user is signed in", "success")
+//                 dispatch({ type: "LOGIN", payload: { user } })
+//             } else {
+//                 dispatch({ type: "LOGOUT" })
 
-                window.toastify("user is signed out", "error")
-            }
-            setIsAppLoader(false)
-        });
-    }, [auth])
-    const [state, dispatch] = useReducer(reducer, initialState)
-    return (
-        <Auth.Provider value={{ ...state, dispatch, isAppLoader, setIsAppLoader }}>
-            {children}
-        </Auth.Provider>
-    )
-}
-
-
+//                 window.toastify("user is signed out", "error")
+//             }
+//             setIsAppLoader(false)
+//         });
+//     }, [auth])
+//     const [state, dispatch] = useReducer(reducer, initialState)
+//     return (
+//         <Auth.Provider value={{ ...state, dispatch, isAppLoader, setIsAppLoader }}>
+//             {children}
+//         </Auth.Provider>
+//     )
+// }
 
 
 
 
 
 
-export const useAuthContext = () => useContext(Auth)
+
+
+// export const useAuthContext = () => useContext(Auth)
